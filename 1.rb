160c9ba88d18eb736cc147284ef0eb467eac9ea1
Phil Pirozhkov <pirjsuka@gmail.com>
2014-08-04 22:33:32 +0400

    wrong way

--- 1.rb
+++ 1.rb
@@ -10,25 +10,35 @@ class CrapCutter
   UNSAFE_TO_UNDEF = {
     Module => [:method_missing, :const_get, :class, :object_id, :__send__, :__id__, :hash, :respond_to?, :module_eval, :class_eval, :to_s, :inspect, :public_instance_method, :public_instance_methods],
     Object => [:method_missing, :const_get, :class, :object_id, :__send__, :__id__, :hash, :respond_to?, :module_eval, :class_eval, :to_s, :inspect],
-    Class => [:method_missing, :const_get, :class, :object_id, :__send__, :__id__, :hash, :respond_to?, :module_eval, :class_eval, :to_s, :inspect, :public_instance_method, :public_instance_methods],
+    Class => [:method_missing, :const_get, :class, :object_id, :__send__, :__id__, :hash, :respond_to?, :module_eval, :class_eval, :to_s, :inspect, :public_instance_method, :public_instance_methods, :instance_methods],
     Binding => #[:method_missing, :instance_variable_set]
     [:method_missing, :const_get, :class, :object_id, :__send__, :__id__, :hash, :respond_to?, :module_eval, :class_eval, :to_s, :inspect, :public_instance_method, :public_instance_methods],
     BasicObject => [:method_missing, :const_get, :class, :object_id, :__send__, :__id__, :hash, :respond_to?, :module_eval, :class_eval, :to_s, :inspect, :public_instance_method, :public_instance_methods],
   }
 
-  def self.wrap_methods constant
-    instance_methods = @@wrapped[constant] ||= {}
+  UNSAFE_ALL = [:method_missing, :const_get, :class, :object_id, :__send__, :__id__, :hash, :respond_to?, :module_eval, :class_eval, :to_s, :inspect, :public_instance_method, :public_instance_methods, :nil?]
 
+  def self.wrap_methods constant
     constant.module_eval do
       # todo: define_method, alias, check if method_missing is present to prevent stack overflow
-      def method_missing method, *args
-        p "called #{method} for #{self}"
-        instance_methods = CrapCutter.wrapped(self.class)
-        if instance_methods.include? method
+
+      if self.instance_methods.include? :method_missing
+        def method_missing method, *args
+          # p "called #{method.inspect} for #{self}"
+          instance_methods = CrapCutter.wrapped(self.class)
+          if instance_methods.include? method
+            instance_methods[method].bind(self).call *args
+          else
+            p "NOT FOUND!"
+            super
+          end
+        end
+      else
+        define_method :method_missing do |method, *args|
+          # p "2called #{method.inspect} for #{self} #{self.class}"
+          instance_methods = CrapCutter.wrapped(self.class)
+          fail NoMethodError, method if instance_methods.include? method
           instance_methods[method].bind(self).call *args
-        else
-          p "NOT FOUND!"
-          super
         end
       end
     end
@@ -37,9 +47,12 @@ class CrapCutter
     # binding.pry
     p [constant, unsafe].join ' '
 
+    instance_methods = @@wrapped[constant] ||= {}
+
     constant.public_instance_methods.each do |method|
-      next if unsafe.include? method
-      # p "undef #{method}"
+      next if !unsafe.nil? && unsafe.include?(method)
+      next if UNSAFE_ALL.include? method
+      p "undef #{method}"
       instance_methods[method] = constant.public_instance_method method
       constant.class_eval "undef #{method}"
     end
@@ -53,8 +66,8 @@ Module.constants
   .map { |constant_name| Module.const_get constant_name }
   .select { |constant| constant.is_a? Class }
   .each do |constant|
-    p constant
-    # p [constant, constant.public_methods.count].join(' ')
+    # p constant
+    p [constant, constant.public_methods.count].join(' ')
     CrapCutter.wrap_methods constant
     p "done"
   end
