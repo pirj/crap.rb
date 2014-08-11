f966da7649c83f41705e9549e1d3928c8509c06a
Phil Pirozhkov <pirjsuka@gmail.com>
2014-08-11 18:34:06 +0400

    running with mspec/rubyspec

--- 1.rb
+++ 1.rb
@@ -4,12 +4,17 @@ class Crap
   UNSAFE = [:const_get, :const_missing, :name, :class, :object_id, :send, :__send__, :__id__, :hash, :respond_to?, :module_eval, :class_eval, :to_s, :inspect,
             :public_instance_method, :public_instance_methods, :instance_methods, :method,
             :nil?, :nonzero?, :===, :constants, :==, :=~, :'||=', :intern, :new, :[], :inspect,
+            :!=, :kind_of?, :!, :untaint, :dup, :instance_variable_set, # MSpec specific: throw away
             :backtrace, :message, :exception, :set_backtrace, # Exceptions specific
-            :delete, # Array specific
-            :include?, # Array specific
-            :p, # Kernel specific
+            :delete, :include?, # Array specific
+            :p, :load, :require, # Kernel specific
+            :include, :const_defined?, # Module specific
+            :extend, :singleton_methods, # Object specific
+            :to_i, # Integer specific
             :round, # Fixnum specific
   ]
+
+  UNSAFE_CONSTANTS = [ IO, File, Dir, Proc, LocalJumpError, SystemStackError, Method, UnboundMethod, Binding, StopIteration, RubyVM, Thread, ThreadGroup, Mutex, Monitor, ThreadError, Fiber, FiberError, TracePoint, Thread::ConditionVariable, Thread::Queue, Thread::SizedQueue]
 
   @@wrapped = {}
 
@@ -19,7 +24,7 @@ class Crap
   end
 
   def self.used clazz, method
-    p ['used', clazz, method]
+    # p ['used', clazz, method]
     clazz_wrapped = @@wrapped[clazz]
     clazz_wrapped.delete method
     unwrap_method clazz, method
@@ -30,6 +35,7 @@ class Crap
   end
 
   def self.wrap constant
+    return if UNSAFE_CONSTANTS.include? constant
     constant.public_instance_methods.each do |method|
       next if UNSAFE.include? method
       next if method =~ /^_/
@@ -44,6 +50,7 @@ class Crap
       alias_method old_method, method
       # p ['wrapping', constant, method]
       define_method(method) do |*args, &block|
+        # p ['called', constant, method]
         # p [constant, method, args.inspect, block.inspect].join '#'
         Crap.used self.class, method
         self.send old_method, *args, &block
@@ -51,6 +58,7 @@ class Crap
     end
   end
 
+  # This method allows wrapping unsafe constants too
   def self.unwrap_method constant, method
     unless constant.instance_methods.include? method
       # p ['wtf', constant, method]
@@ -65,31 +73,21 @@ class Crap
   end
 end
 
-# Module.constants
-#   .map { |constant_name| Module.const_get constant_name }
-#   .select { |constant| constant.is_a? Class }
-
-# [Object, Module, Class, BasicObject, NilClass, Data, TrueClass, FalseClass, Encoding, String, Symbol, Exception, SystemExit, SignalException, Interrupt, StandardError, TypeError, ArgumentError, IndexError, KeyError, RangeError, ScriptError, SyntaxError, LoadError, NotImplementedError, NameError, NoMethodError, RuntimeError, SecurityError, NoMemoryError, EncodingError, SystemCallError, ZeroDivisionError, FloatDomainError, Numeric, Integer, Fixnum, Float, Bignum, Array, Hash, Struct, RegexpError, Regexp, MatchData, Range, IOError, EOFError, Enumerator, Rational, Complex, Date, Set, SortedSet, Time, Random, 
-#! IO, File, Dir, Proc, LocalJumpError, SystemStackError, Method, UnboundMethod, Binding,
-#! StopIteration, RubyVM, Thread, ThreadGroup, Mutex, Monitor,
-#! ThreadError, Fiber, FiberError,
-#! TracePoint,
-#! Thread::ConditionVariable, Thread::Queue, Thread::SizedQueue,
-# ]
-
- 
-[Object, Module, Class, BasicObject, NilClass, Data, TrueClass, FalseClass, Encoding, String, Symbol, Exception, SystemExit, SignalException, Interrupt, StandardError, TypeError, ArgumentError, IndexError, KeyError, RangeError, ScriptError, SyntaxError, LoadError, NotImplementedError, NameError, NoMethodError, RuntimeError, SecurityError, NoMemoryError, EncodingError, SystemCallError, ZeroDivisionError, FloatDomainError, Numeric, Integer, Fixnum, Float, Bignum, RegexpError, Regexp, MatchData, Range, IOError, EOFError, Enumerator, Rational, Complex, Date, Set, SortedSet, Time, Random, Struct, Hash, Array
-].each do |constant|
+Module.constants
+  .map { |constant_name| Module.const_get constant_name }
+  .select { |constant| constant.is_a? Class }
+  .each do |constant|
     Crap.wrap constant
   end
 
-p 1.2+3
-
-def fib x; return 1 if x < 2; fib(x-1) + fib(x-2); end
-
-p fib(5)
-
-p Time.now
+require 'mspec/commands/mspec'
+script = MSpecMain.new
+script.load_default
+# script.load '~/.mspecrc'
+script.options
+script.signals
+script.register
+script.run
 
 File.open('unused.yml', 'w') do |file|
   Crap.unused.each do |clazz, methods|
