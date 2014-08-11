162284ed7568ccd7a8394d2c5ca6bff9b130f267
Phil Pirozhkov <pirjsuka@gmail.com>
2014-08-11 16:28:10 +0400

    a bit more further

--- 1.rb
+++ 1.rb
@@ -1,38 +1,64 @@
 require 'set'
 
 class Crap
-  UNSAFE = [:const_get, :class, :object_id, :send, :__send__, :__id__, :hash, :respond_to?, :module_eval, :class_eval, :to_s, :inspect, :public_instance_method, :public_instance_methods, :nil?, :nonzero?]
+  UNSAFE = [:const_get, :const_missing, :name, :class, :object_id, :send, :__send__, :__id__, :hash, :respond_to?, :module_eval, :class_eval, :to_s, :inspect,
+            :public_instance_method, :public_instance_methods, :instance_methods, :method,
+            :nil?, :nonzero?, :===, :constants, :==, :=~, :'||=', :intern, :new, :[], :inspect,
+            :backtrace, :message, :exception, :set_backtrace, # Exceptions specific
+    :delete, # Array specific
+  ]
 
-  @@calls = {}
+  @@wrapped = {}
 
   def self.register clazz, method
-    clazz_calls = @@calls[clazz] ||= Set.new
-    clazz_calls << method
+    clazz_wrapped = @@wrapped[clazz] ||= Set.new
+    clazz_wrapped << method
   end
 
   def self.used clazz, method
-    clazz_calls = @@calls[clazz] ||= Set.new
-    clazz_calls.delete method
+    # p ['used', clazz, method]
+    clazz_wrapped = @@wrapped[clazz]
+    clazz_wrapped.delete method
+    unwrap_method clazz, method
   end
 
   def self.unused
-    @@calls
+    @@wrapped
   end
 
   def self.wrap constant
     constant.public_instance_methods.each do |method|
-      next if UNSAFE.include?(method)
+      next if UNSAFE.include? method
       next if method =~ /^_/
-      old_method = :"_#{method}"
-      Crap.register constant, method
-      constant.class_eval do
-        alias_method old_method, method
-        define_method(method) do |*args|
-          Crap.used self.class, method
-          self.send old_method, *args
-        end
+      wrap_method constant, method
+    end
+  end
+
+  def self.wrap_method constant, method
+    old_method = :"_#{method}"
+    register constant, method
+    constant.class_eval do
+      alias_method old_method, method
+      # p ['wrapping', constant, method]
+      define_method(method) do |*args, &block|
+        # p [constant, method, args.inspect, block.inspect].join '#'
+        Crap.used self.class, method
+        self.send old_method, *args, &block
       end
     end
+  end
+
+  def self.unwrap_method constant, method
+    unless constant.instance_methods.include? method
+      # p ['wtf', constant, method]
+      return
+    end
+    # p ['undef', constant, method]
+    constant.class_eval <<-EVAL
+      undef #{method}
+      old_method = :"_#{method}"
+      alias_method method, old_method
+    EVAL
   end
 end
 
@@ -40,10 +66,8 @@ end
 #   .map { |constant_name| Module.const_get constant_name }
 #   .select { |constant| constant.is_a? Class }
 
-# [Object, Module, Class, BasicObject, NilClass, Data, TrueClass, FalseClass, Encoding, String, Symbol, Exception, SystemExit, SignalException, Interrupt, StandardError, TypeError, ArgumentError, IndexError, KeyError, RangeError, ScriptError, SyntaxError, LoadError, NotImplementedError, NameError, NoMethodError, RuntimeError, SecurityError, NoMemoryError, EncodingError, SystemCallError, ZeroDivisionError, FloatDomainError, Numeric, Integer, Fixnum, Float, Bignum, Array, Hash, Struct, RegexpError, Regexp, MatchData, Range, IOError, EOFError, IO, File, Dir, Time, Random, Proc, LocalJumpError, SystemStackError, Method, UnboundMethod, Binding, Enumerator, StopIteration, RubyVM, Thread, ThreadGroup, Mutex, ThreadError, Fiber, FiberError, Rational, Complex, TracePoint, Date, Thread::ConditionVariable, Thread::Queue, Thread::SizedQueue, Monitor, Set, SortedSet]
-
-[Numeric, Integer, Fixnum, Float, Bignum]
-  .each do |constant|
+ # [Object, Module, Class, BasicObject, NilClass, Data, TrueClass, FalseClass, Encoding, String, Symbol, Exception, SystemExit, SignalException, Interrupt, StandardError, TypeError, ArgumentError, IndexError, KeyError, RangeError, ScriptError, SyntaxError, LoadError, NotImplementedError, NameError, NoMethodError, RuntimeError, SecurityError, NoMemoryError, EncodingError, SystemCallError, ZeroDivisionError, FloatDomainError, Numeric, Integer, Fixnum, Float, Bignum, Array, Hash, Struct, RegexpError, Regexp, MatchData, Range, IOError, EOFError, IO, File, Dir, Time, Random, Proc, LocalJumpError, SystemStackError, Method, UnboundMethod, Binding, Enumerator, StopIteration, RubyVM, Thread, ThreadGroup, Mutex, ThreadError, Fiber, FiberError, Rational, Complex, TracePoint, Date, Thread::ConditionVariable, Thread::Queue, Thread::SizedQueue, Monitor, Set, SortedSet].each do |constant|
+ [Object, Module, Class, BasicObject, NilClass, Data, TrueClass, FalseClass, Encoding, String, Symbol, Exception, SystemExit, SignalException, Interrupt, StandardError, TypeError, ArgumentError, IndexError, KeyError, RangeError, ScriptError, SyntaxError, LoadError, NotImplementedError, NameError, NoMethodError, RuntimeError, SecurityError, NoMemoryError, EncodingError, SystemCallError, ZeroDivisionError, FloatDomainError, Numeric, Integer, Fixnum, Float, Bignum].each do |constant|
     Crap.wrap constant
   end
 
@@ -51,9 +75,13 @@ p 1.2+3
 
 def fib x; return 1 if x < 2; fib(x-1) + fib(x-2); end
 
-p fib(5)
+p fib(9)
 
-require 'yaml'
 File.open('unused.yml', 'w') do |file|
-  file.write Crap.unused.inject({}) { |acc, kv| acc[kv.first.name] = kv.last.to_a; acc }.to_yaml
+  Crap.unused.each do |clazz, methods|
+    file.write "#{clazz.name}:\n"
+    methods.each do |method|
+      file.write " - #{method}\n"
+    end
+  end
 end
